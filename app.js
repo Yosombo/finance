6d36c8782aebb54d45d36f4ff55fb18cf72aa113
Yosombo <yosomboo@gmail.com>
2020-06-16 18:53:38 -0700

    finance moduluruu item add hiideg bolgoh

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// ----------------- ДЭЛГЭЦТЭЙ ХАРИЛЦАХ ХЭСЭГ
+// ----------------- ДЭЛГЭЦТЭЙ ХАРИЛЦАХ ХЭСЭГ ----------------
 var uiController = (function () {
-  // HTML КЛАССУУДЫГ СОНГОХ ХЭСЭГ privet
+  // HTML КЛАССУУДЫГ СОНГОХ ХЭСЭГ -private-
   var DOMstrings = {
     inputType: ".add__type",
     inputDescription: ".add__description",
@@ -8,11 +8,11 @@ var uiController = (function () {
     addBtn: ".add__btn",
   };
 
-  // ПАБЛИК СЭРВИС БУЮУ ГАДАГШАА ГАРГАХ ОБЕКТ public
+  // ПАБЛИК СЭРВИС БУЮУ ГАДАГШАА ГАРГАХ ОБЕКТ -public-
   return {
     getInput: function () {
       return {
-        type: document.querySelector(DOMstrings.inputType).value,
+        type: document.querySelector(DOMstrings.inputType).value, //exp or inc
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value,
       };
@@ -23,9 +23,9 @@ var uiController = (function () {
   };
 })();
 
-// ----------------- САНХҮҮТЭЙ ХАРИЛЦАХ ХЭСЭГ
+// ----------------- САНХҮҮТЭЙ ХАРИЛЦАХ ХЭСЭГ ----------------
 var financeController = (function () {
-  // ОРЛОГО ЗАРЛАГЫГ ХАДГАЛАХ ОБЕКТ
+  // ОРЛОГО ЗАРЛАГЫГ ХАДГАЛАХ ОБЕКТ -private-
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
@@ -35,27 +35,51 @@ var financeController = (function () {
     this.id = id;
     this.description = description;
     this.value = value;
+  };
+  var data = {
+    items: {
+      inc: [],
+      exp: [],
+    },
 
-    var data = {
-      allitems: {
-        inc: [],
-        exp: [],
-      },
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
 
-      totals: {
-        inc: 0,
-        exp: 0,
-      },
-    };
+  //   PUBLIC SERVICE -public-
+  return {
+    addItem: function (type, desc, val) {
+      var item, id;
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        //type = exp
+        item = new Expense(id, desc, val);
+      }
+      data.items[type].push(item);
+      alert("item added..");
+    },
+    seeData: function () {
+      return data;
+    },
   };
 })();
 
-// ----------------- ПРОГРАММЫН ХОЛБОГЧ ХЭСЭГ
+// ----------------- ПРОГРАММЫН ХОЛБОГЧ ХЭСЭГ ----------------
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
     // 1. ОРУУЛАХ ӨГӨГДӨРЛИЙГ ДЭЛГЭЦЭЭС ОЛЖ АВНА.
-    console.log(uiController.getInput());
+    var input = uiController.getInput();
+
     // 2. ОЛЖ АВСАН ӨГӨГДӨЛҮҮДЭЭ САНХҮҮГИЙН МОДУЛЬ  ХАДГАЛНА.
+    financeController.addItem(input.type, input.description, input.value);
+
     // 3. ОЛЖ АВСАН ӨГӨГЛӨЛҮҮДЭЭ ВЭБ ДЭЭРЭЭ ТОХИРОХ ХЭСЭГТ НЬ ГАРГАНА.
     // 4. ТӨСВИЙГ ТООЦООЛНО.
     // 5. ЭЦСИЙН ҮЛДЭГДЭЛ, ТООЦООГ ДЭЛГЭЦЭНД ГАРГАНА.
